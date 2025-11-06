10d81bb036fbdc963b1751154f4073784d64a3e4
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2025-11-06 14:13:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.5 MB (100.00%)

---
+++ leetcode-solutions/0155-min-stack/solution.cpp
@@ -0,0 +1,29 @@
+// LeetCode: Min Stack
+#include <stack>
+#include <limits>
+using namespace std;
+
+class MinStack {
+    stack<int> st, mn;
+public:
+    MinStack() {}
+
+    void push(int val) {
+        st.push(val);
+        if (mn.empty() || val <= mn.top()) mn.push(val);
+    }
+
+    void pop() {
+        if (st.top() == mn.top()) mn.pop();
+        st.pop();
+    }
+
+    int top() {
+        return st.top();
+    }
+
+    int getMin() {
+        return mn.top();
+    }
+};
+
