a228e84dacf66beb083f752c8c01069143d428c6
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-02-07 18:25:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.1 MB (69.08%)

--- leetcode-solutions/0033-search-in-rotated-sorted-array/solution.cpp
+++ leetcode-solutions/0033-search-in-rotated-sorted-array/solution.cpp
@@ -1,5 +1,5 @@
 // LeetCode: Search in Rotated Sorted Array
-#include <vector>
+/*#include <vector>
 using namespace std;
 
 class Solution {
@@ -18,6 +18,39 @@ public:
                 else r = mid - 1;
             }
         }
+        return -1;
+    }
+};*/
+
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            // left half is sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            // right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+
         return -1;
     }
 };
