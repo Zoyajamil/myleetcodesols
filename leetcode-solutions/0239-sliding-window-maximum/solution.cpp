080afe944d04df04f896de5e62a2bdfc23e1eb70
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-02-15 06:33:23 +0000

    Sync LeetCode submission Runtime - 22 ms (61.60%), Memory - 139.1 MB (74.31%)

---
+++ leetcode-solutions/0239-sliding-window-maximum/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;        // stores indices
+        vector<int> result;  // stores answers
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            // Remove indices out of current window
+            if (!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
+            }
+
+            // Maintain decreasing order in deque
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+
+            // Start adding results after first window
+            if (i >= k - 1) {
+                result.push_back(nums[dq.front()]);
+            }
+        }
+
+        return result;
+    }
+};
+
