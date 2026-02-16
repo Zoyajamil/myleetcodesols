dc19620a0c7b5b9165dedd845c083fb7adac5544
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-02-16 18:35:02 +0000

    Sync LeetCode submission Runtime - 4 ms (100.00%), Memory - 23.2 MB (100.00%)

--- leetcode-solutions/0155-min-stack/solution.cpp
+++ leetcode-solutions/0155-min-stack/solution.cpp
@@ -1,29 +1,160 @@
-// LeetCode: Min Stack
-#include <stack>
-#include <limits>
-using namespace std;
+/*class MinStack {
+
+private:
+    stack<int> st;
+    int mini;
+
+public:
+    MinStack() {
+    }
+    
+    void push(int value) {
+        
+        //if stack is empty
+        if(st.empty()){
+            //update the minimum value
+            mini=value;
+            
+            st.push(value);
+            return;
+    }
+
+    if(value>mini){
+        st.push(value);
+    }else{
+        //add modified value to the stack 
+        st.push(2*value-mini);
+        mini=value;
+    }
+    }
+
+    //POP functino
+    void pop(){
+        if(st.empty()) return -1;
+
+        int x=st.top();
+        st.pop();
+
+        if(x<mini){
+            mini=2*mini-x; //update the minimim //previous element 
+        }
+    }
+
+    int top(){
+        if(st.empty()) return -1;
+        int x=st.top();
+        
+        if(mini<x){
+            return x;
+        }else{
+            return mini;
+        }
+    }
+
+        int getMin(){
+            if(st.empty()) return -1;
+            return mini;
+        }
+
+};*/
+
+
+
+
+
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
 
 class MinStack {
-    stack<int> st, mn;
+private:
+    // Initialize a stack
+    stack<long long> st;
+
+    // To store the minimum value 
+    long long mini;
+
 public:
-    MinStack() {}
-
-    void push(int val) {
-        st.push(val);
-        if (mn.empty() || val <= mn.top()) mn.push(val);
+    
+    // Empty Constructor
+    MinStack() {
     }
-
+    
+    // Method to push a value in stack
+    void push(int value) {
+        
+        // If stack is empty
+        if (st.empty()) {
+            // Update the minimum value
+            mini = value;
+            
+            // Push current value as minimum
+            st.push(value);
+            return;
+        }
+        
+        // If the value is greater than the minimum
+        if (value > mini) {
+            st.push(value);
+        }
+        else {
+            // Add the modified value to stack
+            st.push(2LL * value - mini);
+            
+            // Update the minimum
+            mini = value;
+        }
+    }
+    
+    // Method to pop a value from stack
     void pop() {
-        if (st.top() == mn.top()) mn.pop();
-        st.pop();
+        
+        // Base case
+        if (st.empty()) return;
+        
+        // Get the top
+        long long x = st.top();
+        st.pop(); // Pop operation
+        
+        // If the modified value was added to stack
+        if (x < mini) {
+            // Update the minimum
+            mini = 2LL * mini - x;
+        }
     }
-
+    
+    // Method to get the top of stack
     int top() {
-        return st.top();
+        
+        // Base case
+        if (st.empty()) return -1;
+        
+        // Get the top
+        long long x = st.top();
+        
+        // Return top if minimum is less than the top
+        if (x > mini) return x;
+        
+        // Otherwise return mini
+        return mini;
     }
-
+    
+    // Method to get the minimum in stack
     int getMin() {
-        return mn.top();
+        
+        // Base case
+        if (st.empty()) return -1;
+        
+        // Return the minimum
+        return mini;
     }
 };
+
 
