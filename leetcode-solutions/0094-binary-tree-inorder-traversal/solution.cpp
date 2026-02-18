437613753512210d18d0d1b76bf108904b4f3fde
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-02-18 18:29:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.1 MB (12.76%)

--- leetcode-solutions/0094-binary-tree-inorder-traversal/solution.cpp
+++ leetcode-solutions/0094-binary-tree-inorder-traversal/solution.cpp
@@ -1,20 +1,54 @@
-// LeetCode: Binary Tree Inorder Traversal
-#include <vector>
-using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+*/ 
+//Define the TreeNode structure
+
 
 class Solution {
 public:
-    void dfs(TreeNode* root, vector<int>& res) {
-        if (!root) return;
-        dfs(root->left, res);
-        res.push_back(root->val);
-        dfs(root->right, res);
-    }
 
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> res;
-        dfs(root, res);
-        return res;
+// Function to perform inorder traversal
+// of a binary tree iteratively
+vector<int> inorderTraversal(TreeNode* root){
+    stack<TreeNode*> st;
+    TreeNode* node = root;
+    vector<int> inorder;
+
+
+    while(true){
+        // If the current node is not NULL
+        if(node != NULL){
+            // Push the current
+            // node to the stack
+            st.push(node);
+            // Move to the left child
+            // of the current node
+            node = node->left;
+        }
+        else{
+            // If the stack is empty,
+            // break the loop
+            if(st.empty()){
+                break;
+            }
+            
+            node = st.top();
+            st.pop();
+            inorder.push_back(node->val);
+            node = node->right;
+        }
     }
+    // Return the inorder
+    // traversal result
+    return inorder;
+}
+
 };
-
