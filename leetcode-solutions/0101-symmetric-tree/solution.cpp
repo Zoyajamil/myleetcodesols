52606e7e92fd3374be82599828a18324a802510a
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-02-23 18:11:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.3 MB (94.22%)

--- leetcode-solutions/0101-symmetric-tree/solution.cpp
+++ leetcode-solutions/0101-symmetric-tree/solution.cpp
@@ -17,11 +17,12 @@ public:
     }
 
 private: 
-    bool ismirror(TreeNode* t1,TreeNode* t2){
-        if(!t1 || !t2)
-            return t1==t2;
+    bool ismirror(TreeNode* node1,TreeNode* node2){
+        if(!node1 && !node2) return true;
 
-        return (t1->val==t2->val) && ismirror(t1->left,t2->right)  && 
-        ismirror(t1->right,t2->left);
-        } 
+        if(!node1 || !node2) return false;
+
+        return (node1->val==node2->val) && ismirror(node1->left,node2->right) 
+        && ismirror(node1->right,node2->left);
+    }
 };
