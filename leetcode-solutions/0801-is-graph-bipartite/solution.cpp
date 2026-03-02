8a95bbcf24d62312eb4f929ceb3c5634d1d3b9a1
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-03-02 07:57:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.4 MB (58.81%)

---
+++ leetcode-solutions/0801-is-graph-bipartite/solution.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool check(int start,vector<vector<int>>& graph,int color[]){
+        int V=graph.size();
+        queue<int> q;
+        q.push(start);
+
+        color[start]=0;
+
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+
+            for(auto it : graph[node]){
+                if(color[it]==-1){
+
+                    color[it]=!color[node];
+                    q.push(it);
+                }
+
+                else if(color[it]==color[node]){
+                    return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        int V=graph.size();
+        int color[V];
+
+        for(int i=0;i<V;i++)
+            color[i]=-1;
+
+            //handle components 
+            for(int i=0;i<V;i++){
+                if(color[i]==-1){
+                    if(check(i,graph,color)==false)
+                        return false;
+                }
+            }
+            return true;
+        }
+};
