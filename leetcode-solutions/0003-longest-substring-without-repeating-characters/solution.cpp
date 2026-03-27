a71d179b6ee77e7cf235b1a338a9ac3f21fcc847
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-03-27 19:48:53 +0000

    Sync LeetCode submission Runtime - 4 ms (84.67%), Memory - 11.9 MB (78.76%)

--- leetcode-solutions/0003-longest-substring-without-repeating-characters/solution.cpp
+++ leetcode-solutions/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,64 +1,21 @@
-/*class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        vector<int> mpp(256, -1);
-        int left = 0, right = 0;
-        int n = s.size();
-        int len = 0;
-        while (right < n) {
-            if (mpp[s[right]] != -1)
-                left = max(mpp[s[right]] + 1, left);
-
-            mpp[s[right]] = right;
-
-            len = max(len, right - left + 1);
-            right++;
-        }
-        return len;
-    }
-};*/
-
-
-/*class solution{
-public:
-    int lengthofLongestsubstring(string s){
-        vector<int> mpp(256,-1);
-        int left=0,rigth=0;
-        int n=s.size();
-        int len=0;
-        while (rigth < n){
-            if(mpp[s[rigth]]!=-1)
-                left= max(mpp[s[right]]+1,left);  //variable overloaded 
-                //to move the left sliding pointer directly to the repeating characters index(one place next to the index)
-            
-            mpp[s[right]]=right; //update the map index of the variable
-
-            len=max(len,right-left+1);
-            right++;
-        }
-        return len;
-    }
-};*/
-
-
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> mpp(256, -1); // map for last index of each character
-        int left = 0, right = 0;
-        int n = s.size();
-        int len = 0;
+        unordered_map<char, int> mp;
+        int left = 0;
+        int maxLen = 0;
 
-        while (right < n) {
-            if (mpp[s[right]] != -1)
-                left = max(mpp[s[right]] + 1, left); // move left to avoid repeat
+        for (int right = 0; right < s.length(); right++) {
 
-            mpp[s[right]] = right; // update character’s latest index
-            len = max(len, right - left + 1);
-            right++;
+            if (mp.find(s[right]) != mp.end()) {
+                left = max(left, mp[s[right]] + 1);
+            }
+
+            mp[s[right]] = right;
+
+            maxLen = max(maxLen, right - left + 1);
         }
 
-        return len;
+        return maxLen;
     }
 };
-
