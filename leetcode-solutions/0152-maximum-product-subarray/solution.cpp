5a1172e93383462f7d68d7acad97cce2d9639cdf
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-04-03 17:28:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (87.19%)

--- leetcode-solutions/0152-maximum-product-subarray/solution.cpp
+++ leetcode-solutions/0152-maximum-product-subarray/solution.cpp
@@ -1,53 +1,21 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n = nums.size();
+        int res=nums[0];
+        int maxproduct=nums[0];
+        int minproduct=nums[0];
 
-        int pre = 1, suff = 1;
-        int ans = INT_MIN;
+        for(int i=1;i<nums.size();i++){
+            int curr=nums[i];
 
-        for (int i = 0; i < n; i++) {
+            if(curr<0) swap(maxproduct,minproduct);
 
-            if (pre == 0) pre = 1;
-            if (suff == 0) suff = 1;
+            maxproduct=max(curr,maxproduct * curr);
+            minproduct=min(curr,minproduct * curr);
 
-           
-            pre *= nums[i];
-            suff *= nums[n - i - 1];
+            res=max(res,maxproduct);
 
-
-            ans = max(ans, max(pre, suff));
         }
-
-        return ans;
-    }
-};
-
-
-
-/*class Solution {
-public:
-    int maxProduct(vector<int>& nums) {
-        int res = nums[0];
-        int maxProd = nums[0];
-        int minProd = nums[0];
-
-        for (int i = 1; i < nums.size(); i++) {
-            int curr = nums[i];
-
-            // If negative, swap because sign flips
-            if (curr < 0) {
-                swap(maxProd, minProd);
-            }
-
-            // Update max and min product ending here
-            maxProd = max(curr, maxProd * curr);
-            minProd = min(curr, minProd * curr);
-
-            // Update global maximum
-            res = max(res, maxProd);
-        }
-
         return res;
     }
-};*/
+};
