8766c8d3193233949e092ff12d6a171393f740e9
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-04-04 07:28:23 +0000

    Sync LeetCode submission Runtime - 52 ms (33.74%), Memory - 28.9 MB (92.42%)

--- leetcode-solutions/0015-3sum/solution.cpp
+++ leetcode-solutions/0015-3sum/solution.cpp
@@ -6,56 +6,46 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;      // This will store all unique triplets
-        sort(nums.begin(), nums.end()); // Step 1: Sort array to make duplicate handling + 2-pointer possible
-        int n = nums.size();
+   vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
 
-        // Loop for first element of the triplet
-        for (int i = 0; i < n - 2; ++i) {
-
-            // Skip duplicate values for the first element
-            if (i > 0 && nums[i] == nums[i - 1])
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i] ==nums[i-1])
                 continue;
 
-            int l = i + 1;     // Left pointer
-            int r = n - 1;     // Right pointer
+            int l=i+1;
+            int r=n-1;
 
-            // Now use 2-pointer technique to find pairs that sum to -nums[i]
-            while (l < r) {
+            while(l<r){
+                long sum=(long)nums[i]+ nums[l] + nums[r];
 
-                long sum = (long)nums[i] + nums[l] + nums[r];  
-                // Long used to avoid integer overflow
+                if(sum==0){
+                    res.push_back({nums[i],nums[l],nums[r]});
 
-                if (sum == 0) {
-                    // Found a valid triplet
-                    res.push_back({nums[i], nums[l], nums[r]});
-
-                    // Skip duplicates for the second element
-                    while (l < r && nums[l] == nums[l + 1])
-                        ++l;
-
-                    // Skip duplicates for the third element
-                    while (l < r && nums[r] == nums[r - 1])
+                    while(l<r && nums[l]==nums[l+1])
+                        l++;
+                    
+                    while(l<r && nums[r]==nums[r-1])
                         --r;
 
-                    // Move both pointers to find new pairs
                     ++l;
                     --r;
                 }
 
-                else if (sum < 0) {
-                    // Sum is too small → increase it by moving left pointer to right
+                else if(sum<0){
                     ++l;
-                }
-
-                else {
-                    // Sum is too large → decrease it by moving right pointer to left
+                }else{
                     --r;
                 }
             }
         }
-        return res;   // Return all unique triplets
+
+        return res;
+
     }
 };
 
+
+//[-4, -1, -1, 0, 1, 2]
