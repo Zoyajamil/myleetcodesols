c790d9496efac03ca40303a9b65faa91b996fc0f
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-04-08 18:21:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.1 MB (26.76%)

--- leetcode-solutions/0908-middle-of-the-linked-list/solution.cpp
+++ leetcode-solutions/0908-middle-of-the-linked-list/solution.cpp
@@ -7,30 +7,17 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
- *//**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
  */
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
+        ListNode* slow=head;
+        ListNode* fast=head;
 
-        // Tortoise & Hare Algorithm
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;        // move slow one step
-            fast = fast->next->next;  // move fast two steps
+        while(fast!=NULL && fast->next !=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-
-        return slow; // slow is now at the middle node
+        return slow;
     }
 };
-
-
