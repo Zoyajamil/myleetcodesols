c3d75fc1a23c1374bf3b5c688b63270ac655acd3
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-04-16 21:35:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.8 MB (89.30%)

--- leetcode-solutions/0094-binary-tree-inorder-traversal/solution.cpp
+++ leetcode-solutions/0094-binary-tree-inorder-traversal/solution.cpp
@@ -8,98 +8,33 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
-*/ 
-//Define the TreeNode structure
-
-
-/*class Solution {
+ */
+class Solution {
 public:
-
-// Function to perform inorder traversal
-// of a binary tree iteratively
-vector<int> inorderTraversal(TreeNode* root){
-    stack<TreeNode*> st;
-    TreeNode* node = root;
-    vector<int> inorder;
-
-
-    while(true){
-        // If the current node is not NULL
-        if(node != NULL){
-            // Push the current
-            // node to the stack
-            st.push(node);
-            // Move to the left child
-            // of the current node
-            node = node->left;
-        }
-        else{
-            // If the stack is empty,
-            // break the loop
-            if(st.empty()){
-                break;
-            }
-            
-            node = st.top();
-            st.pop();
-            inorder.push_back(node->val);
-            node = node->right;
-        }
-    }
-    // Return the inorder
-    // traversal result
-    return inorder;
-}
-
-};*/
-
-#include <vector>
-using namespace std;
-
-class Solution{
-public:
-
-    vector<int> inorderTraversal(TreeNode* root){
-    
-    vector<int> inorder;
-    TreeNode* cur = root;
-
-    while(cur!=NULL){
-
-        //case 1 no left child
-        if(cur->left ==NULL){
-            inorder.push_back(cur->val);
-            cur=cur->right;
-        }
-
-        else{
-            //find inorder predecessor
-            TreeNode* prev=cur->left;
-
-
-            while(prev->right!=NULL && prev->right!=cur){
-                prev=prev->right;
-            }
-
-            //make temporary thread
-            if(prev->right==NULL){
-                prev->right=cur;
-                cur=cur->left;
-            }
-
-            else{
-                //remove thread 
-                prev->right=NULL;
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        TreeNode *cur=root;
+        while(cur!=NULL){
+            if(cur->left==NULL){
                 inorder.push_back(cur->val);
                 cur=cur->right;
             }
+            else{
+                TreeNode *prev=cur->left;
+                while(prev->right && prev->right!=cur){
+                    prev=prev->right;
+                }
 
+                if(prev->right==NULL){
+                    prev->right=cur;
+                    cur=cur->left;
+                }else{
+                    prev->right=NULL;
+                    inorder.push_back(cur->val);
+                    cur=cur->right;
+                }
+            }
         }
+        return inorder;
     }
-
-    return inorder;
-    }
-
 };
-
-
