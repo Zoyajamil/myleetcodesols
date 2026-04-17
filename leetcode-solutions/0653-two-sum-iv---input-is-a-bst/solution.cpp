f26e341c70b17842afacea7fe62a3d3d0a2255f6
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-04-17 15:09:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 37.3 MB (45.72%)

--- leetcode-solutions/0653-two-sum-iv---input-is-a-bst/solution.cpp
+++ leetcode-solutions/0653-two-sum-iv---input-is-a-bst/solution.cpp
@@ -11,71 +11,90 @@
  */
 
 
+/*class Solution {
+public:
+    unordered_set<int> st;
+
+    bool findTarget(TreeNode* root, int k) {
+        // Base case: null node
+        if (root == NULL) return false;
+
+        // If complement is found, return true
+        if (st.count(k - root->val)) return true;
+
+        // Otherwise, add current node value to the set
+        st.insert(root->val);
+
+        // Recurse on left and right subtrees
+        return findTarget(root->left, k) || findTarget(root->right, k);
+    }
+};*/
+
+
+//Another approach - Do inorder traversal and then use two pointers left and right like standard two sum and check
+
+//Most optimal approach is using a BST Iterator with functions next() and before()
+
+
 class BSTIterator{
-private:
-    stack<TreeNode*> st;
-    bool reverse;
-    void pushall(TreeNode* node){
-        while(node){
-            st.push(node);
-            if(reverse)
+    stack<TreeNode*> mystack;
+    //using reverse as a flag
+    //reverse ->true->before
+    //reverse->false->next
+    bool reverse=true;
+public: 
+    BSTIterator(TreeNode *root, bool isReverse){
+        reverse=isReverse;
+        pushAll(root);
+    }
+
+    //return whether we have next smallest number or not 
+    bool hasnext(){
+        return !mystack.empty();
+    }
+
+    //return the next smallest number
+    int next(){
+        TreeNode *tempnode=mystack.top();
+        mystack.pop();
+        if(!reverse) pushAll(tempnode->right);
+        else pushAll(tempnode->left);
+        return tempnode->val;
+    }
+
+private: 
+    void pushAll(TreeNode *node){
+        for(;node!=NULL;){
+            mystack.push(node);
+            if(reverse==true){
                 node=node->right;
-            else
+            }else{
                 node=node->left;
+            }
         }
     }
-
-public:
-
-    BSTIterator(TreeNode* root,bool isReverse){
-        reverse = isReverse;
-        pushall(root);
-    }
-
-    int next(){
-        TreeNode* node=st.top();
-        st.pop();
-
-        if(!reverse)
-            pushall(node->right);
-        else
-            pushall(node->left);
-        
-        return node->val;
-    }
-
-    bool hasnext(){
-        return !st.empty();
-    }
 };
-
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
         if(!root) return false;
 
-        BSTIterator left(root,false); //smallest to largest
-        BSTIterator right(root,true); //largest to smallest
+        //next
+        BSTIterator l(root,false);
+        //for before
+        BSTIterator r(root,true);
 
-        int i=left.next();
-        int j=right.next();
+        int i =l.next();
+        int j =r.next();
 
         while(i<j){
             if(i+j==k) return true;
-            else if(i+j<k){
-                if(left.hasnext())
-                    i=left.next();
-                else
-                return false;
-            }
-            else{
-                if(right.hasnext())
-                    j=right.next();
-                else
-                return false;
-            }
+            else if(i+j <k) i=l.next();
+            else j=r.next();
         }
 
         return false;
     }
 };
+
+
