bf442c7a4e8fb193af362490405c772636ca7c36
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-06-17 12:58:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.4 MB (5.69%)

--- leetcode-solutions/0152-maximum-product-subarray/solution.cpp
+++ leetcode-solutions/0152-maximum-product-subarray/solution.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int res=nums[0];
-        int maxproduct=nums[0];
-        int minproduct=nums[0];
 
-        for(int i=1;i<nums.size();i++){
-            int curr=nums[i];
+        int n = nums.size();
 
-            if(curr<0) swap(maxproduct,minproduct);
+        // DP arrays for max and min product ending at i
+        vector<int> dpMax(n);
+        vector<int> dpMin(n);
 
-            maxproduct=max(curr,maxproduct * curr);
-            minproduct=min(curr,minproduct * curr);
+        // Base case
+        dpMax[0] = nums[0];
+        dpMin[0] = nums[0];
 
-            res=max(res,maxproduct);
+        // Store final answer
+        int ans = nums[0];
 
+        for (int i = 1; i < n; i++) {
+
+            // Max product ending at i
+            dpMax[i] = max({
+                nums[i],                    // start new subarray
+                nums[i] * dpMax[i - 1],     // extend max product
+                nums[i] * dpMin[i - 1]      // extend min product
+            });
+
+            // Min product ending at i
+            dpMin[i] = min({
+                nums[i],                    // start new subarray
+                nums[i] * dpMax[i - 1],     // extend max product
+                nums[i] * dpMin[i - 1]      // extend min product
+            });
+
+            // Update overall maximum
+            ans = max(ans, dpMax[i]);
         }
-        return res;
+
+        return ans;
     }
 };
