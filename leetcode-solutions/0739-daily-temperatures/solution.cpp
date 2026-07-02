2c8981715529799c6f33ce1402ac4387a96808e4
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-07-02 09:01:48 +0000

    Sync LeetCode submission Runtime - 20 ms (63.82%), Memory - 102.9 MB (78.38%)

---
+++ leetcode-solutions/0739-daily-temperatures/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n=temp.size();
+        vector<int> result(n,0);
+        stack<int> st;
+
+        for(int i=n-1; i>=0;i--){
+
+            while(!st.empty() && temp[i]>=temp[st.top()]){
+                st.pop();
+            }
+
+            if(!st.empty()){
+                result[i]= st.top()-i;
+            }
+
+            st.push(i);
+        }
+
+        return result;
+    }
+};
