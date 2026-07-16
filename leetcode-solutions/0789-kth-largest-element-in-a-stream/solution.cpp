a6e84e92614d3799a900138a37eb08efb9347c86
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-07-16 10:41:46 +0000

    Sync LeetCode submission Runtime - 7 ms (61.98%), Memory - 33.3 MB (25.50%)

---
+++ leetcode-solutions/0789-kth-largest-element-in-a-stream/solution.cpp
@@ -0,0 +1,27 @@
+class KthLargest {
+public:
+    
+    int k;
+    priority_queue<int, vector<int>, greater<int>> pq; 
+
+    KthLargest(int k, vector<int>& nums) {
+        this->k=k;
+        for(auto ele : nums){  //putting elements in the queue
+            pq.push(ele);
+            if(pq.size() > k) pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size() > k) pq.pop();
+        return pq.top();
+        
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
