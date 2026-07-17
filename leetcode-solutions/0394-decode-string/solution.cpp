f1a296b002d7d4738ca9f2586baf82026f9f1413
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-07-17 17:55:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.4 MB (55.68%)

--- leetcode-solutions/0394-decode-string/solution.cpp
+++ leetcode-solutions/0394-decode-string/solution.cpp
@@ -1,35 +1,60 @@
-// LeetCode: Decode String
-#include <string>
-#include <stack>
-using namespace std;
-
 class Solution {
 public:
     string decodeString(string s) {
-        stack<int> countSt;
-        stack<string> strSt;
-        string curr = "";
-        int num = 0;
+        if(s.length()==0) return s;
 
-        for (char c : s) {
-            if (isdigit(c)) {
-                num = num * 10 + (c - '0');
-            } else if (c == '[') {
-                countSt.push(num);
-                strSt.push(curr);
-                num = 0;
-                curr = "";
-            } else if (c == ']') {
-                int times = countSt.top(); countSt.pop();
-                string prev = strSt.top(); strSt.pop();
-                string tmp;
-                for (int i = 0; i < times; i++) tmp += curr;
-                curr = prev + tmp;
-            } else {
-                curr.push_back(c);
+        string ans="";
+        int num=0;
+        stack<int> numstck;
+        stack<string> strstck;
+
+        for(auto &x : s){
+            if(x>='0' && x<='9'){
+                num= (num * 10) + (x-'0');  //to convert string ASCII char to a number 
+            }
+
+            else{
+                if(x=='['){
+                    numstck.push(num); //if num add to numstack
+                    num=0;
+                    strstck.push("[");
+                }
+
+                else if(x==']'){
+                    string temp="";
+
+                    while(!strstck.empty() && strstck.top()!="["){ //pop until ] and add to temp
+                        temp= strstck.top() + temp;
+                        strstck.pop();
+                    }
+
+                    strstck.pop();  //remove '['
+
+                    int repeat=numstck.top(); //take the number
+                    numstck.pop();
+
+                    string tempans="";
+                    //repeat the string n no of times 
+                    for(int i=0; i<repeat; i++){
+                        tempans+=temp;
+                    }
+
+                    strstck.push(tempans);
+                }
+
+                else{  //a normal character is seen here 
+                    string temp="";
+                    temp+=x;
+                    strstck.push(temp);
+                }
             }
         }
-        return curr;
+
+        while(!strstck.empty()){
+            ans=strstck.top() + ans;
+            strstck.pop(); 
+        }
+    return ans;
+
     }
 };
-
