6c0924343da032025ba0b6af026d23dbf3ed5963
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-07-27 12:07:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (88.55%)

---
+++ leetcode-solutions/0070-climbing-stairs/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1 || n==2) return n;
+        int prev1=2;
+        int prev2=1;
+
+        int result = prev1;
+
+        for(int i=3;i<=n;i++){
+            result = prev1+prev2;
+            prev2=prev1;
+            prev1=result;
+        }
+        return result;
+    }
+};
