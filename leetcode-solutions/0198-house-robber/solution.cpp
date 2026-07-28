cef7be536346fdccc2f4591a52a03cd32caa2d57
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-07-28 18:16:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.9 MB (6.12%)

--- leetcode-solutions/0198-house-robber/solution.cpp
+++ leetcode-solutions/0198-house-robber/solution.cpp
@@ -1,21 +1,21 @@
-
-
+// S5. DP Iterative (Bottom-up / Tabulation) version
+// Time: O(n), Space: O(n)
 class Solution {
 public:
-
-    int f(int ind,vector<int> &nums, vector<int> &dp){
-        if(ind==0) return nums[ind];
-        if(ind<0) return 0;
-
-        if(dp[ind]!=-1) return dp[ind];
-        int pick= nums[ind] + f(ind-2,nums,dp);
-        int notpick= 0+ f(ind-1,nums,dp);
-
-        return dp[ind]=max(pick,notpick);
-    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return f(n-1,nums,dp);
+        vector<int> dp(n);
+
+        if(n==1) return nums[0];
+
+        dp[0]=nums[0];
+        dp[1]=max(nums[0], nums[1]);
+
+        for(int i=2;i<n;i++){
+            dp[i]= max(dp[i-2] + nums[i] , dp[i-1]);
+        }
+
+        return dp[n-1];
     }
 };
+
