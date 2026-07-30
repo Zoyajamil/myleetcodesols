e6c8b938b47f6258cd14e9b630384a9c62ed9f53
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-07-30 11:46:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.5 MB (5.22%)

--- leetcode-solutions/0062-unique-paths/solution.cpp
+++ leetcode-solutions/0062-unique-paths/solution.cpp
@@ -1,25 +1,25 @@
-// Problem: Unique Paths (LeetCode 62)
-// Link: https://leetcode.com/problems/unique-paths/
-
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        // Total steps needed = (m-1) downs + (n-1) rights = m+n-2
-        int N = n + m - 2;
+        vector<vector<int>> dp(m, vector<int>(n,0));
 
-        // Choose (m-1) down moves (or equivalently (n-1) right moves)
-        int r = m - 1;
-
-        // Use formula for nCr = (N choose r)
-        double res = 1;
-
-        // Compute nCr iteratively to avoid overflow
-        for (int i = 1; i <= r; i++) {
-            res = res * (N - r + i) / i;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                if(i>0){
+                    dp[i][j]+= dp[i-1][j];
+                }
+                if(j>0){
+                    dp[i][j]+=dp[i][j-1];
+                }
+            }
         }
 
-        // Result must be integer (unique paths count)
-        return (int)res;
+        return dp[m-1][n-1];
     }
 };
+
 
