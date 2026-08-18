ca0802b2b5ef9fd4d2d4759b474993c38f7332d7
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-08-18 13:06:34 +0000

    Sync LeetCode submission Runtime - 1 ms (59.02%), Memory - 55.2 MB (19.75%)

--- leetcode-solutions/0041-first-missing-positive/solution.cpp
+++ leetcode-solutions/0041-first-missing-positive/solution.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n = nums.size();
-
-        for(int i = 0; i < n; i++) {
-            if(nums[i] <= 0 || nums[i] > n) {
-                nums[i] = n + 1;
+        int n=nums.size();
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]>n || nums[i]<=0){
+                nums[i]= n+1;
             }
-        }
+        } 
 
-        for(int i = 0; i < n; i++) {
+
+        for(int i=0;i<n;i++){
             int num = abs(nums[i]);
 
-            if(num <= n) {
-                nums[num - 1] = -abs(nums[num - 1]);
+            if(num <= n){
+                nums[num-1]= -1 * abs(nums[num-1]);
             }
         }
 
-        for(int i = 0; i < n; i++) {
-            if(nums[i] > 0) {
-                return i + 1;
+        for(int i=0;i<n;i++){
+            if(nums[i] > 0){
+                return i+1;
             }
         }
-
-        return n + 1;
+        return n+1;
     }
 };
