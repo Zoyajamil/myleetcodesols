7edf042fe7d71b364103f9fea1a3791d88940326
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-08-18 13:14:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.9 MB (50.61%)

--- leetcode-solutions/0119-pascals-triangle-ii/solution.cpp
+++ leetcode-solutions/0119-pascals-triangle-ii/solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    std::vector<int> getRow(int rowIndex) {
-        std::vector<int> res(1,1);
+    vector<int> getRow(int rowindex) {
+        std :: vector<int> res(1,1);
         long long prev=1;
 
-        for(int i=1;i<=rowIndex;i++){
-            long long ans=prev * (rowIndex-i+1)/i;
+        for(int i=1;i<=rowindex;i++){
+            long long ans=prev * (rowindex-i+1) / i;
             res.push_back(ans);
             prev=ans;
         }
-
         return res;
     }
 };
