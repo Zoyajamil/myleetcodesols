1ce0383cadc425cbc1ba6d0c3c1ea6d9ac775fa1
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-08-18 08:10:43 +0000

    Sync LeetCode submission Runtime - 61 ms (32.37%), Memory - 50.9 MB (18.98%)

--- leetcode-solutions/0560-subarray-sum-equals-k/solution.cpp
+++ leetcode-solutions/0560-subarray-sum-equals-k/solution.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> mpp;
-        int prefixsum=0; 
         int count=0;
-        mpp[0]=1;
-
+        unordered_map<int,int> mpp;
+        int prefixsum=0;
         int n=nums.size();
+
+
+        mpp[0]=1;
 
         for(int i=0;i<n;i++){
             prefixsum+=nums[i];
             int remove=prefixsum-k;
             count+=mpp[remove];
             mpp[prefixsum]+=1;
-            
         }
         return count;
     }
