79370ca0c4a272da103ef201ee3f85675743b4df
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-08-19 18:31:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (70.69%)

---
+++ leetcode-solutions/0567-permutation-in-string/solution.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    bool arevectorsequal(vector<int> &a, vector<int> &b){
+        for(int i=0;i<26;i++){
+            if(a[i] != b[i]) return false;
+        }
+        return true;
+    }
+public:
+
+    bool checkInclusion(string s1, string s2) {
+        if(s2.size() < s1.size()) return false;
+
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
+
+        for(char c : s1){
+            freq1[c-'a']++;
+        }
+
+        int i=0;
+        int j=0;
+
+        while(j<s2.size()){
+            freq2[s2[j]-'a']++;
+
+            if(j-i+1 == s1.size()){
+                if(arevectorsequal(freq1,freq2)) return true;
+            }
+        
+
+            if(j-i+1 < s1.size()) j++;
+            else{
+                freq2[s2[i]-'a']--;
+                i++;
+                j++;
+            } 
+        }
+        return false;
+    }
+};
