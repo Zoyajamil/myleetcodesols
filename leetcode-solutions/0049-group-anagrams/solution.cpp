46ff1e2d621800627b98c7a8f010d2ac18831f93
Zoyajamil <147808863+Zoyajamil@users.noreply.github.com>
2026-08-23 15:35:14 +0000

    Sync LeetCode submission Runtime - 16 ms (59.31%), Memory - 26.2 MB (41.66%)

--- leetcode-solutions/0049-group-anagrams/solution.cpp
+++ leetcode-solutions/0049-group-anagrams/solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        
-        for(auto x: strs){
-            string word = x;
-            sort(word.begin(), word.end());
-            mp[word].push_back(x);
+        unordered_map<string, vector<string>> mpp;
+
+        for(auto x : strs){
+            string word=x;
+            sort(word.begin() , word.end());
+            mpp[word].push_back(x);
         }
-        
+
         vector<vector<string>> ans;
-        for(auto x: mp){
+
+        for(auto x : mpp){
             ans.push_back(x.second);
         }
+        
         return ans;
     }
 };
